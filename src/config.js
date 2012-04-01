386e583288a2cd3c49efa79e5e13beacad883e17
Oleg Efimov <efimovov@gmail.com>
2012-04-01 05:38:30 +0400

    Fix config

--- src/config.js
+++ src/config.js
@@ -10,8 +10,6 @@ CREATE DATABASE test DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 GRANT ALL ON test.* TO test@localhost IDENTIFIED BY "";
 */
 
-
-
 exports.getConfig = function (factor) {
   var cfg = {
     // Database connection settings
@@ -25,7 +23,7 @@ exports.getConfig = function (factor) {
     escape_count: 1000000 * factor,
     string_to_escape: "str\\str\"str\'str\x00str",
     reconnect_count: 1000 * factor,
-    insert_rows_count: 100000 * factor,
+    insert_rows_count: 10000 * factor,
 
     // Delay before assertion check (ms)
     delay_before_select: 1000
@@ -33,12 +31,12 @@ exports.getConfig = function (factor) {
 
   cfg.create_table_query = "CREATE TABLE " + cfg.test_table +
                            " (alpha INTEGER, beta VARCHAR(128), pi FLOAT) " +
-                           "TYPE=MEMORY;";
+                           "ENGINE=MEMORY;";
 
   cfg.insert_query = "INSERT INTO " + cfg.test_table +
                      " VALUES (1, 'hello', 3.141);";
 
-  cfg.selected_row_example = {alpha: 1, beta: 'hello', pi: 3.141};
+  cfg.selected_row_example = {alpha: 1, beta: 'hello', pi: 3.1415926};
 
   return cfg;
 };
