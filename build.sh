aec869b74b302bc6312e5a1bd9e4b77bb37f5de4
Oleg Efimov <efimovov@gmail.com>
2014-12-21 02:04:47 +0300

    Travis build fixes

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 mkdir -p ./build
 
 if [[ "$OSTYPE" =~ ^darwin ]]; then
-  CC -O2 -std=c++0x -stdlib=libc++ -o ./build/benchmark-cpp ./src/other/benchmark.cc -lmysqlclient -lssl      -lz -lc++
+  g++ -O2 -std=c++0x -stdlib=libc++ -o ./build/benchmark-cpp ./src/other/benchmark.cc -lmysqlclient -lssl      -lz -lc++
 else
-  CC -O2 -std=c++0x                -o ./build/benchmark-cpp ./src/other/benchmark.cc -lmysqlclient -lssl -lrt -lz
+  g++ -O2 -std=c++0x                -o ./build/benchmark-cpp ./src/other/benchmark.cc -lmysqlclient -lssl -lrt -lz
 fi
